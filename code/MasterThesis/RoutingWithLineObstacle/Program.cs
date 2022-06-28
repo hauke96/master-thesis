f2f99478c37a10dcf68d0d8faf2fb0003069138b
Hauke Stieler <mail@hauke-stieler.de>
2022-06-28 13:36:22 +0200

    Add empty project for routing with line obstacle

---
+++ code/MasterThesis/RoutingWithLineObstacle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using Mars.Components.Starter;
+using Mars.Interfaces.Model;
+
+namespace RoutingWithLineObstacle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+            // SharedEnvironment.Init();
+            // Target.SetRandomPosition();
+
+            var description = new ModelDescription();
+            // description.AddLayer<Layer.Layer>();
+            // description.AddAgent<Agent, Layer.Layer>();
+
+            var file = File.ReadAllText("config.json");
+            var config = SimulationConfig.Deserialize(file);
+
+            var task = SimulationStarter.Start(description, config);
+            var loopResults = task.Run();
+
+            Console.WriteLine($"Simulation execution finished after {loopResults.Iterations} steps");
+        }
+    }
+}
