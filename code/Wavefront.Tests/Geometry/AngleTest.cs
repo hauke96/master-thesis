3a6c82932d9d9f7c986082c557aff39055b20642
Hauke Stieler <mail@hauke-stieler.de>
2023-01-27 13:37:55 +0100

    Slightly adjust AngleTest

--- code/Wavefront.Tests/Geometry/AngleTest.cs
+++ code/Wavefront.Tests/Geometry/AngleTest.cs
@@ -31,15 +31,16 @@ public class AngleTest
         Assert.True(Angle.IsBetweenEqual(0, 90, 90));
         Assert.True(Angle.IsBetweenEqual(90, 90 - 0.0001, 360));
         Assert.True(Angle.IsBetweenEqual(269.99236701900003, 180.00763299072972, 180.00763298898323));
-        Assert.True(Angle.IsBetweenEqual(0, 360, 360));
         Assert.True(Angle.IsBetweenEqual(10, 10, 10));
         Assert.True(Angle.IsBetweenEqual(270, 180, 180));
         Assert.True(Angle.IsBetweenEqual(270, 180, 179.9999));
+        Assert.True(Angle.IsBetweenEqual(180, 0, 0));
 
         Assert.False(Angle.IsBetweenEqual(0, 90, 45));
         Assert.False(Angle.IsBetweenEqual(45, 20, 45));
         Assert.False(Angle.IsBetweenEqual(10, 339, 10));
         Assert.False(Angle.IsBetweenEqual(100, 0, 360));
+        Assert.False(Angle.IsBetweenEqual(180, 90, 0));
     }
 
     [Test]
