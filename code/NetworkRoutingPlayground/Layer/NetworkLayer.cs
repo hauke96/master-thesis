9c8181400bea114b89b2b8edf3a67873657d7360
Hauke Stieler <mail@hauke-stieler.de>
2023-03-19 15:00:59 +0100

    Merge highways into visibility graph

--- code/NetworkRoutingPlayground/Layer/NetworkLayer.cs
+++ code/NetworkRoutingPlayground/Layer/NetworkLayer.cs
@@ -9,9 +9,11 @@ using Mars.Interfaces.Environments;
 using Mars.Interfaces.Layers;
 using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
+using NetTopologySuite.Index.Quadtree;
 using NetTopologySuite.Index.Strtree;
 using NetTopologySuite.IO;
 using ServiceStack;
+using ServiceStack.Text;
 using Wavefront;
 using Wavefront.Geometry;
 using Feature = NetTopologySuite.Features.Feature;
@@ -31,8 +33,6 @@ public class NetworkLayer : VectorLayer
         var vertexNeighbors = GetObstacleNeighbors();
 
         var nodeNeighbors = AddVisibilityVerticesAndEdges(vertexNeighbors, graph);
-
-        // TODO Split edges in half when they intersect with each other and add a node at the intersection point.
 
         Environment = new SpatialGraphEnvironment(graph);
 
@@ -129,14 +129,162 @@ public class NetworkLayer : VectorLayer
         Console.WriteLine($"  Number of nodes: {graph.NodesMap.Count}");
         Console.WriteLine($"  Number of edges: {graph.EdgesMap.Count}");
 
+        watch.Restart();
+        AddRoadsToGraph(graph);
+
+        Console.WriteLine($"NetworkLayer: Splitting graph edges done after {watch.ElapsedMilliseconds}ms");
+        Console.WriteLine($"  Number of nodes: {graph.NodesMap.Count}");
+        Console.WriteLine($"  Number of edges: {graph.EdgesMap.Count}");
+
         return nodeNeighbors;
     }
 
     private void AddRoadsToGraph(SpatialGraph graph)
     {
-        var index = new STRtree<Feature>();
+        // Create and fill a spatial index with all edges of the graph
+        var index = new Quadtree<int>();
+        graph.Edges.Values.Each((i, e) =>
+        {
+            var envelope = GetEnvelopeOfEdge(e);
+            index.Insert(envelope, i);
+        });
 
-        Features.Where(f =>
+        var roadFeatures = GetAllRoadFeatures();
+        var roadLineStrings = SplitFeaturesToSegments(roadFeatures);
+
+        roadLineStrings
+            .Each(roadEdgeFeature =>
+            {
+                var roadFeatureFrom = roadEdgeFeature.Geometry.Coordinates[0];
+                var roadFeatureTo = roadEdgeFeature.Geometry.Coordinates[1];
+
+                // Get all IDs of visibility edges that truly intersect the road edge. 
+                var edgeIds = index.Query(roadEdgeFeature.Geometry.EnvelopeInternal).Where(id =>
+                {
+                    var edgePositions = graph.Edges[id].Geometry.Map(p => p.ToCoordinate());
+                    return Intersect.DoIntersect(roadFeatureFrom, roadFeatureTo, edgePositions[0], edgePositions[1]);
+                }).ToList();
+
+                if (edgeIds.Any())
+                {
+                    // We have edges that intersect the given road segment. This means we have to split the edge and the
+                    // road segment at all intersection points and create new nodes and edges. 
+
+                    var roadEdgeLineSegment = new LineSegment(
+                        roadEdgeFeature.Geometry.Coordinates[0],
+                        roadEdgeFeature.Geometry.Coordinates[1]
+                    );
+
+                    // Every edge from the edgeIds list is definitely intersecting our road segment. This means for each
+                    // edge a new node will be created. To remember what node was created at what location (to connect
+                    // the nodes), this map stores this information. It also prevent creating duplicate node since all
+                    // visibility edges were added twice (forward and backward direction).
+                    var intersectionNodeMap = new Dictionary<Coordinate, int>();
+
+                    edgeIds.Each(visibilityEdgeId =>
+                    {
+                        var visibilityEdgeLineSegment = new LineSegment(
+                            graph.Edges[visibilityEdgeId].Geometry[0].ToCoordinate(),
+                            graph.Edges[visibilityEdgeId].Geometry[1].ToCoordinate()
+                        );
+                        var visibilityEdge = graph.Edges[visibilityEdgeId];
+
+                        var intersectionCoordinate = roadEdgeLineSegment.Intersection(visibilityEdgeLineSegment);
+
+                        // 1. Add intersection node
+                        var intersectionNode = -1;
+                        if (intersectionNodeMap.ContainsKey(intersectionCoordinate))
+                        {
+                            intersectionNode = intersectionNodeMap[intersectionCoordinate];
+                        }
+                        else
+                        {
+                            intersectionNode = graph.AddNode(new Dictionary<string, object>
+                            {
+                                { "x", intersectionCoordinate.X },
+                                { "y", intersectionCoordinate.Y },
+                            }).Key;
+                            intersectionNodeMap.Add(intersectionCoordinate, intersectionNode);
+                        }
+
+                        // 2. Remove old visibility edge
+                        index.Remove(GetEnvelopeOfEdge(graph.Edges[visibilityEdgeId]), visibilityEdgeId);
+                        graph.RemoveEdge(visibilityEdgeId);
+
+                        // 3. Add two new edges
+                        var newEdge = graph.AddEdge(visibilityEdge.From, intersectionNode);
+                        index.Insert(GetEnvelopeOfEdge(newEdge), newEdge.Key);
+
+                        newEdge = graph.AddEdge(intersectionNode, visibilityEdge.To);
+                        index.Insert(GetEnvelopeOfEdge(newEdge), newEdge.Key);
+                    });
+
+                    // 4. Add new line segments for our whole road segment
+                    var roadFeatureFromPosition = roadFeatureFrom.ToPosition();
+                    var roadFeatureToPosition = roadFeatureTo.ToPosition();
+                    var orderedNodeIds = intersectionNodeMap.Values
+                        .OrderBy(nodeId => graph.NodesMap[nodeId].Position.DistanceInMTo(roadFeatureFromPosition))
+                        .ToList();
+
+                    var fromNode = graph.NodesMap.Values
+                        .MinBy(node => node.Position.DistanceInMTo(roadFeatureFromPosition));
+                    if (fromNode.Position.DistanceInMTo(roadFeatureFromPosition) > 0.1)
+                    {
+                        fromNode = graph.AddNode(roadFeatureFromPosition.X, roadFeatureFromPosition.Y);
+                    }
+
+                    var toNode = graph.NodesMap.Values
+                        .MinBy(node => node.Position.DistanceInMTo(roadFeatureToPosition));
+                    if (toNode.Position.DistanceInMTo(roadFeatureToPosition) > 0.1)
+                    {
+                        toNode = graph.AddNode(roadFeatureToPosition.X, roadFeatureToPosition.Y);
+                    }
+
+                    graph.AddEdge(fromNode.Key, orderedNodeIds[0]);
+                    graph.AddEdge(orderedNodeIds[0], fromNode.Key);
+
+                    for (int i = 0; i < orderedNodeIds.Count - 1; i++)
+                    {
+                        graph.AddEdge(orderedNodeIds[i], orderedNodeIds[i + 1]);
+                        graph.AddEdge(orderedNodeIds[i + 1], orderedNodeIds[i]);
+                    }
+
+                    graph.AddEdge(toNode.Key, orderedNodeIds[^1]);
+                    graph.AddEdge(orderedNodeIds[^1], toNode.Key);
+                }
+                else
+                {
+                    // TODO handle empty result
+                }
+            });
+    }
+
+    /// <returns>A list of all segments from the given features. Which means, each of these line strings has only two coordinates.</returns>
+    private static List<Feature> SplitFeaturesToSegments(IEnumerable<IVectorFeature> roadFeatures)
+    {
+        return roadFeatures
+            // Turn each highway edge into a list of line strings with only two coordinates. This makes it easier to
+            // split them at intersection points with visibility edges.
+            .Map(f =>
+            {
+                var features = new List<Feature>();
+                var coordinates = f.VectorStructured.Geometry.Coordinates;
+                for (int i = 0; i < coordinates.Length - 1; i++)
+                {
+                    features.Add(new Feature(new LineString(new[] { coordinates[i], coordinates[i + 1] }),
+                        f.VectorStructured.Attributes));
+                }
+
+                return features;
+            })
+            .SelectMany(x => x)
+            .ToList();
+    }
+
+    private IEnumerable<IVectorFeature> GetAllRoadFeatures()
+    {
+        return Features
+            .Where(f =>
             {
                 return f.VectorStructured.Attributes.GetNames().Any(name =>
                 {
@@ -144,41 +292,22 @@ public class NetworkLayer : VectorLayer
                     return f.VectorStructured.Geometry is LineString ||
                            lowerName.Contains("highway");
                 });
-            })
-            .Map(f => new Feature(f.VectorStructured.Geometry, f.VectorStructured.Attributes))
-            .Each(f => index.Insert(f.Geometry.EnvelopeInternal, f));
+            });
+    }
 
-        graph.Edges.Values.Each((i, e) => 
-        {
-            Feature feature;
+    /// <summary>
+    /// This assumes that the edge only consists of two coordinates.
+    /// </summary>
+    private static Envelope GetEnvelopeOfEdge(EdgeData e)
+    {
+        var coordinates = e.Geometry;
 
-            // if (e.Geometry[0].Equals(e.Geometry[^1]))
-            // {
-            //     // Closed geometry
-            //     feature = new Feature(
-            //         new Polygon(
-            //             new LinearRing(
-            //                 e.Geometry.Map(p => p.ToCoordinate()).ToArray()
-            //             )
-            //         ),
-            //         new AttributesTable()
-            //     );
-            // }
-            // else
-            // {
-            //     // Open LineString
-                feature = new Feature(
-                    new LineString(
-                        e.Geometry.Map(p => p.ToCoordinate()).ToArray()
-                    ),
-                    new AttributesTable()
-                );
-            // }
-            // index.Insert(feature.Geometry.EnvelopeInternal, feature);
-            
-            index.Query(feature.Geometry.EnvelopeInternal)
-                .Whe
-        });
+        var minX = Math.Min(coordinates[0].X, coordinates[1].X);
+        var maxX = Math.Max(coordinates[0].X, coordinates[1].X);
+        var minY = Math.Min(coordinates[0].Y, coordinates[1].Y);
+        var maxY = Math.Max(coordinates[0].Y, coordinates[1].Y);
+
+        return new Envelope(minX, maxX, minY, maxY);
     }
 
     private void WriteGraphToFile(SpatialGraph graph, Dictionary<int, List<int>> nodeNeighbors)
@@ -196,7 +325,8 @@ public class NetworkLayer : VectorLayer
                     new AttributesTable(new Dictionary<string, object>()
                     {
                         { "node_id", key },
-                        { "neighbors", nodeNeighbors[key] }
+                        // nodeNeighbors are not up to date anymore due to splitting of the graph
+                        // { "neighbors", nodeNeighbors[key] }
                     })));
             });
             graph.Edges.Values.Each((key, edgeData) =>
