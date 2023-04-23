e9fb018078170e5d7c06d7de184ff9a1cfc828d9
Hauke Stieler <mail@hauke-stieler.de>
2023-04-23 15:25:47 +0200

    Add additional DoIntersectOrTouch method and extend tests

--- code/HybridVisibilityGraphRouting.Tests/Geometry/IntersectTest.cs
+++ code/HybridVisibilityGraphRouting.Tests/Geometry/IntersectTest.cs
@@ -11,11 +11,15 @@ public class IntersectTest
     {
         Assert.AreEqual(0, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(1, 1), new Coordinate(2, 2)));
         Assert.AreEqual(0, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(2, 0)));
+        Assert.AreEqual(0, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 0)));
         Assert.AreEqual(0, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(0, 1), new Coordinate(0, 2)));
+        Assert.AreEqual(0, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(0, 2), new Coordinate(0, 1)));
 
+        Assert.AreEqual(-1, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(0, 1), new Coordinate(1, 1)));
         Assert.AreEqual(-1, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(1, 1), new Coordinate(2, 0)));
 
-        Assert.AreEqual(1, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(2, 1)));
+        Assert.AreEqual(1, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(0, 1), new Coordinate(-1, 1)));
+        Assert.AreEqual(1, Intersect.Orientation(new Coordinate(0, 0), new Coordinate(1, 1), new Coordinate(0, 2)));
     }
 
     [Test]
@@ -69,5 +73,51 @@ public class IntersectTest
             new Coordinate(7, 4)));
         Assert.False(Intersect.DoIntersect(new Coordinate(3, 5), new Coordinate(1, 5), new Coordinate(2, 5),
             new Coordinate(2, 10)));
+    }
+
+    [Test]
+    public void DoIntersectOrTouch()
+    {
+        // Intersecting
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 2), new Coordinate(0, 2),
+            new Coordinate(2, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 2), new Coordinate(2, 0), new Coordinate(0, 0),
+            new Coordinate(2, 2)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 1),
+            new Coordinate(2, -1)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(1, 1), new Coordinate(2, -1), new Coordinate(0, 0),
+            new Coordinate(2, 0)));
+
+        // Touching
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 1),
+            new Coordinate(1, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 0),
+            new Coordinate(1, 1)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(1, 0), new Coordinate(2, 0), new Coordinate(1, 0),
+            new Coordinate(2, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(1, 0), new Coordinate(2, 0), new Coordinate(0, 0),
+            new Coordinate(1, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(1, 0),
+            new Coordinate(1, 1)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(1, 1),
+            new Coordinate(1, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(1, 0), new Coordinate(1, 0),
+            new Coordinate(2, 0)));
+        Assert.True(Intersect.DoIntersectOrTouch(new Coordinate(3, 5), new Coordinate(1, 5), new Coordinate(2, 5),
+            new Coordinate(2, 10)));
+
+        // Not intersecting/touching
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 1),
+            new Coordinate(2, 1)));
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(1, 2),
+            new Coordinate(1, 1)));
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(0, 1),
+            new Coordinate(2, 1)));
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(0, 0), new Coordinate(0, 2), new Coordinate(1, 0),
+            new Coordinate(1, 2)));
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(2, 1), new Coordinate(3, 2), new Coordinate(3, 1),
+            new Coordinate(3, 0)));
+        Assert.False(Intersect.DoIntersectOrTouch(new Coordinate(3, 1), new Coordinate(3, 0), new Coordinate(2, 1),
+            new Coordinate(3, 2)));
     }
 }
--- code/HybridVisibilityGraphRouting/Geometry/Intersect.cs
+++ code/HybridVisibilityGraphRouting/Geometry/Intersect.cs
@@ -20,7 +20,7 @@ namespace HybridVisibilityGraphRouting.Geometry
         }
 
         /// <summary>
-        /// Precondition: The segments p, q and r are collinear (-> Orientation(p, q, r) should return 0).
+        /// Precondition: The segments p, q and r are collinear (-> Orientation(p, q, r) == 0).
         ///
         /// <param name="orientation">The orientation of p to the other coordinates. Only a collinear p (with means
         /// orientation == 0) can be on the line segment.</param>
@@ -33,6 +33,17 @@ namespace HybridVisibilityGraphRouting.Geometry
                    Math.Min(start.Y, end.Y) <= p.Y && p.Y <= Math.Max(start.Y, end.Y);
         }
 
+        /// <summary>
+        /// Determines the orientation between the line segments p0p1 and p0p2. This means it determines if the line
+        /// string p0p1p2 turns right or left at p1.
+        /// </summary>
+        /// <returns>
+        /// <ul>
+        /// <li>Returns -1, if p0p2 is counterclockwise to p0p1</li>
+        /// <li>Returns 1, if p0p2 is clockwise to p0p1</li>
+        /// <li>Returns 0, if p0, p1 and p2 are collinear (???)</li>
+        /// </ul>
+        /// </returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int Orientation(Coordinate p0, Coordinate p1, Coordinate p2)
         {
@@ -78,10 +89,57 @@ namespace HybridVisibilityGraphRouting.Geometry
                 return false;
             }
 
-            return !(IsOnSegment(start2, end2, start1, orientation1) ||
-                     IsOnSegment(start2, end2, end1, orientation2) ||
-                     IsOnSegment(start1, end1, start2, orientation3) ||
-                     IsOnSegment(start1, end1, end2, orientation4));
+            return !AreTouching(start1, end1, start2, end2, orientation1, orientation2, orientation3, orientation4);
+        }
+
+        public static bool DoIntersectOrTouch(Coordinate start1, Coordinate end1, Coordinate start2, Coordinate end2)
+        {
+            if (
+                start1.X > start2.X && start1.X > end2.X && end1.X > start2.X && end1.X > end2.X ||
+                start1.X < start2.X && start1.X < end2.X && end1.X < start2.X && end1.X < end2.X ||
+                start1.Y > start2.Y && start1.Y > end2.Y && end1.Y > start2.Y && end1.Y > end2.Y ||
+                start1.Y < start2.Y && start1.Y < end2.Y && end1.Y < start2.Y && end1.Y < end2.Y
+            )
+            {
+                return false;
+            }
+
+            if (start1.Equals(start2) || end1.Equals(end2) ||
+                start1.Equals(end2) || end1.Equals(start2))
+            {
+                return true;
+            }
+
+            var orientation1 = Orientation(start2, end2, start1);
+            var orientation2 = Orientation(start2, end2, end1);
+            var orientation3 = Orientation(start1, end1, start2);
+            var orientation4 = Orientation(start1, end1, end2);
+
+            if (AreTouching(start1, end1, start2, end2, orientation1, orientation2, orientation3, orientation4))
+            {
+                return true;
+            }
+            
+            if (orientation1 == orientation2)
+            {
+                return false;
+            }
+            
+            if (orientation3 == orientation4)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool AreTouching(Coordinate start1, Coordinate end1, Coordinate start2, Coordinate end2,
+            int orientation1, int orientation2, int orientation3, int orientation4)
+        {
+            return IsOnSegment(start2, end2, start1, orientation1) ||
+                   IsOnSegment(start2, end2, end1, orientation2) ||
+                   IsOnSegment(start1, end1, start2, orientation3) ||
+                   IsOnSegment(start1, end1, end2, orientation4);
         }
     }
 }
