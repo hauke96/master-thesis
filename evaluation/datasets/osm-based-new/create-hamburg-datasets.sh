0cd69dc3dbcd23f674a9c3fa4c459b7a85e278eb
Hauke Stieler <mail@hauke-stieler.de>
2023-06-06 20:09:21 +0200

    Restructure osm-based-new dataset folder

--- evaluation/datasets/osm-based-new/create-hamburg-datasets.sh
+++ evaluation/datasets/osm-based-new/create-hamburg-datasets.sh
@@ -2,30 +2,45 @@
 
 set -e
 
-function clip()
+function process()
 {
-    ogr2ogr "hamburg-$1km2.geojson" hamburg-latest-filtered.geojson -clipsrc "bbox-$1km2.geojson" -overwrite
+    OUTDIR="./dataset-hamburg/$1km2"
+    mkdir -p $OUTDIR
+    ogr2ogr "$OUTDIR/data.geojson" hamburg-latest-filtered.geojson -clipsrc "./bbox/$1km2.geojson" -overwrite
+    cp "./waypoints/$1km2.geojson" "$OUTDIR/waypoints.geojson"
 }
 
 echo "Clean up existing files"
-rm -f hamburg-latest.osm.pbf \
+rm -rf hamburg-latest.osm.pbf \
+    hamburg-latest-clipped.osm.pbf \
     hamburg-latest.geojson \
     hamburg-latest-filtered.geojson \
-    hamburg-*km2.geojson
+    dataset-hamburg
 
 echo "Download raw OSM-dump"
 wget https://download.geofabrik.de/europe/germany/hamburg-latest.osm.pbf
 
+echo "Clip PBF by largest BBOX file (4km2)"
+osmium extract --polygon ./bbox/4km2.geojson -o hamburg-latest-clipped.osm.pbf hamburg-latest.osm.pbf
+
 echo "Convert PBF to GeoJSON"
-osmium export hamburg-latest.osm.pbf -o hamburg-latest.geojson
+osmium export hamburg-latest-clipped.osm.pbf -o hamburg-latest.geojson
 
 echo "Extract features that are not underground"
 ogr2ogr hamburg-latest-filtered.geojson hamburg-latest.geojson -where "\"level\" IS NULL OR \"level\"='0'" -overwrite
 
 echo "Extract feature within BBOXes"
-clip "0,5"
-clip "1"
-clip "1,5"
-clip "2"
-clip "3"
-clip "4"
+process "0,5"
+process "1"
+process "1,5"
+process "2"
+process "3"
+process "4"
+
+echo "Clean up intermediate files"
+rm -rf hamburg-latest.osm.pbf \
+    hamburg-latest-clipped.osm.pbf \
+    hamburg-latest.geojson \
+    hamburg-latest-filtered.geojson
+
+echo "Done"
