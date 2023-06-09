c39036acb612b9dbe72a532b9292338f697b962c
Hauke Stieler <mail@hauke-stieler.de>
2023-06-09 16:49:53 +0200

    Adjust dataset creation script and respective README.md

--- evaluation/datasets/osm-based/README.md
+++ evaluation/datasets/osm-based/README.md
@@ -2,13 +2,17 @@ This folder contains the OSM based datasets and their waypoints.
 Each dataset file covers a certain area, e.g. the `3km2` file covers a 3 km² large area.
 For each such files exists a waypoint file, e.g. `waypoints-3km2.geojson` for the mentiones dataset file.
 
+Additionally, several types of datasets can be created, e.g. the "city" and "rural" types.
+They are structures in the same way, just their location is different so that they cover different types of landscapes and obstacles.
+
 # Creation
 
-The files can be (re)created using the `create-hamburg-dataset.sh` script.
+The files can be (re)created using the `create-datasets.sh` script.
 It automatically downloads an OSM dump of Hamburg, Germany, and directly cuts it into the needed dataset files.
 
-The script create sub-folders for each dataset file because the execution script for the evaluation expects *one* `waypoints.geojson` file for all datasets.
-This is not the case here since each dataset file has its own waypoint file.
+The script create sub-folders for each dataset type.
+In each type-folder (e.g. "dataset-city") sub-folder for each area exist, because the execution script for the evaluation expects *one* `waypoints.geojson` file for all datasets.
+This is not the case here since each dataset file has its own waypoint file, hence separate folders are needed.
 
 ## Filtering
 
@@ -20,5 +24,6 @@ This is done due to the fact that the hybrid routing algorithm has no special ha
 Some external tools are needed for this script:
 
 * `wget`
+* 'grep'
 * `osmium`
 * `ogr2ogr`
--- evaluation/datasets/osm-based/create-datasets.sh
+++ evaluation/datasets/osm-based/create-datasets.sh
@@ -8,38 +8,54 @@ TYPE="$1"
 # $2: Specific bbox- and waypoint file (e.g. "3km2")
 function process()
 {
-	echo "Process $2km2 dataset fpr '$1'"
+	echo "Process $2km2 dataset for '$1'"
     OUTDIR="./dataset-$1/$2km2"
     mkdir -p $OUTDIR
     ogr2ogr "$OUTDIR/$2km2.geojson" hamburg-latest-filtered.geojson -clipsrc "./bbox-$1/$2km2.geojson" -overwrite
     cp "./waypoints-$1/$2km2.geojson" "$OUTDIR/waypoints.geojson"
 }
 
-echo "Clean up existing files"
-rm -rf hamburg-latest.osm.pbf \
-    hamburg-latest-clipped.osm.pbf \
-    hamburg-latest.geojson \
-    hamburg-latest-filtered.geojson \
-    dataset-hamburg
+function cleanup-intermediate()
+{
+	echo "Clean up intermediate files"
+	rm -rf hamburg-latest-clipped.osm.pbf \
+		hamburg-latest.geojson \
+	    hamburg-latest-filtered.geojson
+}
 
-echo "Download raw OSM-dump"
-wget https://download.geofabrik.de/europe/germany/hamburg-latest.osm.pbf
-echo
+cleanup-intermediate
 
-for TYPE in "city rural"
+if [ ! -f ./hamburg-latest.osm.pbf ]
+then
+	echo "Download raw OSM-extract"
+	wget https://download.geofabrik.de/europe/germany/hamburg-latest.osm.pbf
+else
+	echo "Re-use existing extract file"
+fi
+
+for TYPE in "city" "rural"
 do
 	echo "=========="
 	echo "  $TYPE"
 	echo "=========="
+
+	echo "Clean up existing dataset folder"
+	rm -rf "./dataset-$TYPE"
 	
 	echo "Clip PBF by largest BBOX file (4km2)"
-	osmium extract --polygon "./bbox-$TYPE/4km2.geojson" -o hamburg-latest-clipped.osm.pbf hamburg-latest.osm.pbf
+	osmium extract --polygon "./bbox-$TYPE/4km2.geojson" -o hamburg-latest-clipped.osm.pbf hamburg-latest.osm.pbf --overwrite
 	
 	echo "Convert PBF to GeoJSON"
-	osmium export hamburg-latest-clipped.osm.pbf -o hamburg-latest.geojson
-	
-	echo "Extract features that are not underground"
-	ogr2ogr hamburg-latest-filtered.geojson hamburg-latest.geojson -where "\"level\" IS NULL OR \"level\"='0'" -overwrite
+	osmium export hamburg-latest-clipped.osm.pbf -o hamburg-latest.geojson --overwrite
+
+	if grep -q "\"level\":" hamburg-latest.geojson
+	then
+		echo "Extract features that are not underground"
+		ogr2ogr hamburg-latest-filtered.geojson hamburg-latest.geojson -where "\"level\" IS NULL OR \"level\"='0'" -overwrite
+	else
+		echo "No 'level' in GeoJSON-file -> no filtering"
+		cp hamburg-latest.geojson hamburg-latest-filtered.geojson
+	fi
 	
 	echo "Extract feature within BBOXes"
 	process $TYPE "0,5"
@@ -49,14 +65,11 @@ do
 	process $TYPE "3"
 	process $TYPE "4"
 	
+	cleanup-intermediate
+
 	echo "Created datasets for '$TYPE'"
 	echo
 done
-	
-echo "Clean up intermediate files"
-rm -rf hamburg-latest.osm.pbf \
-    hamburg-latest-clipped.osm.pbf \
-    hamburg-latest.geojson \
-    hamburg-latest-filtered.geojson
+echo
 
 echo "Done"
