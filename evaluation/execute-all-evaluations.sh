bc21d35722a7e456c88a276ac570a33d291f180c
Hauke Stieler <mail@hauke-stieler.de>
2023-07-05 18:27:50 +0200

    Add new measurement results

--- evaluation/execute-all-evaluations.sh
+++ evaluation/execute-all-evaluations.sh
@@ -18,9 +18,9 @@ echo
 echo
 
 # Rectangle datasets
-head "Rectangle pattern datasets"
-DATASETS=$(ls datasets/pattern-based-rectangles/ | grep --color=never -P "pattern_\\d*x\\d*\.geojson" | sed "s/\.geojson//g")
-./execute-evaluation.sh ../code/HikerModel datasets/pattern-based-rectangles/ results/pattern-based-rectangles "$DATASETS"
+#head "Rectangle pattern datasets"
+#DATASETS=$(ls datasets/pattern-based-rectangles/ | grep --color=never -P "pattern_\\d*x\\d*\.geojson" | sed "s/\.geojson//g")
+#./execute-evaluation.sh ../code/HikerModel datasets/pattern-based-rectangles/ results/pattern-based-rectangles "$DATASETS"
 
 # Maze datasets
 head "Maze pattern datasets"
@@ -28,25 +28,25 @@ DATASETS=$(ls datasets/pattern-based-maze/ | grep --color=never -P "pattern_\\d*
 ./execute-evaluation.sh ../code/HikerModel datasets/pattern-based-maze/ results/pattern-based-maze "$DATASETS"
 
 # Circle datasets
-head "Circle pattern datasets"
-DATASETS=$(ls datasets/pattern-based-circles/ | grep --color=never -P "pattern_\\d*x\\d*\.geojson" | sed "s/\.geojson//g")
-./execute-evaluation.sh ../code/HikerModel datasets/pattern-based-circles/ results/pattern-based-circles "$DATASETS"
+#head "Circle pattern datasets"
+#DATASETS=$(ls datasets/pattern-based-circles/ | grep --color=never -P "pattern_\\d*x\\d*\.geojson" | sed "s/\.geojson//g")
+#./execute-evaluation.sh ../code/HikerModel datasets/pattern-based-circles/ results/pattern-based-circles "$DATASETS"
 
 # OSM "city"
-head "OSM \"city\" datasets"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/0,5km2/ results/osm-based-city "0,5km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/1km2/ results/osm-based-city "1km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/1,5km2/ results/osm-based-city "1,5km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/2km2/ results/osm-based-city "2km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/3km2/ results/osm-based-city "3km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/4km2/ results/osm-based-city "4km2"
+#head "OSM \"city\" datasets"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/0,5km2/ results/osm-based-city "0,5km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/1km2/ results/osm-based-city "1km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/1,5km2/ results/osm-based-city "1,5km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/2km2/ results/osm-based-city "2km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/3km2/ results/osm-based-city "3km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-city/4km2/ results/osm-based-city "4km2"
 
 # OSM "rural"
 head "OSM \"rural\" datasets"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/0,5km2/ results/osm-based-rural "0,5km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/1km2/ results/osm-based-rural "1km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/1,5km2/ results/osm-based-rural "1,5km2"
-./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/2km2/ results/osm-based-rural "2km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/0,5km2/ results/osm-based-rural "0,5km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/1km2/ results/osm-based-rural "1km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/1,5km2/ results/osm-based-rural "1,5km2"
+#./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/2km2/ results/osm-based-rural "2km2"
 ./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/3km2/ results/osm-based-rural "3km2"
 ./execute-evaluation.sh ../code/HikerModel datasets/osm-based/dataset-rural/4km2/ results/osm-based-rural "4km2"
 
