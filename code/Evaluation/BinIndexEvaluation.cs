c2de8e5bca0945b03cdcea91e17cbfb6e3d749b7
Hauke Stieler <mail@hauke-stieler.de>
2023-07-16 15:07:27 +0200

    Adjust BinIndex evaluation script

--- code/Evaluation/BinIndexEvaluation.cs
+++ code/Evaluation/BinIndexEvaluation.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using HybridVisibilityGraphRouting.Geometry;
 using HybridVisibilityGraphRouting.Index;
 using NetTopologySuite.Algorithm;
-using NetTopologySuite.Geometries;
 using NetTopologySuite.Index.Bintree;
 
 namespace Evaluation;
@@ -18,7 +17,7 @@ public class BinIndexEvaluation
         Console.WriteLine("Create intervals");
         var intervals = new List<(double, double)>();
         var random = new Random();
-        for (var i = 0; i < 100_000; i++)
+        for (var i = 0; i < 200_000; i++)
         {
             intervals.Add((random.NextDouble()*100, random.NextDouble()*100));
         }
@@ -98,7 +97,7 @@ public class BinIndexEvaluation
         
         name = "Bintree (querying)";
         stopwatch.Start();
-        for (var i = 0; i < intervals.Count/100 - 1; i++)
+        for (var i = 0; i < intervals.Count - 1; i++)
         {
             binTree.Query(intervals[i].Item1);
             binTree.Query((intervals[i].Item1+intervals[i].Item2)/2);
@@ -106,18 +105,12 @@ public class BinIndexEvaluation
         }
         stopwatch.Stop();
         // Console.WriteLine($"{name} : {stopwatch.ElapsedMilliseconds}");
-        measurements[name] = measurements.TryGetValue(name, out measurement) ? measurement + GetMicroseconds(stopwatch.ElapsedTicks*100) : GetMicroseconds(stopwatch.ElapsedTicks*100);
+        measurements[name] = measurements.TryGetValue(name, out measurement) ? measurement + GetMicroseconds(stopwatch.ElapsedTicks) : GetMicroseconds(stopwatch.ElapsedTicks);
         stopwatch.Reset();
     }
 
-    private static int GetMicroseconds(long ticks)
+    private static long GetMicroseconds(long ticks)
     {
-        return (int)(ticks / (double)Stopwatch.Frequency * 1_000_000);
-    }
-
-    public static LineSegment NewLineSegment(int seed)
-    {
-        var r = new Random(seed);
-        return new LineSegment(r.NextDouble(), r.NextDouble(), r.NextDouble(), r.NextDouble());
+        return (long)(ticks / (double)Stopwatch.Frequency * 1_000_000d);
     }
 }
