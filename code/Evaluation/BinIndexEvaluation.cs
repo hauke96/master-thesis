31caeb3495ca33c3644a9c7f6ee519776974ae96
Hauke Stieler <mail@hauke-stieler.de>
2023-07-21 10:56:09 +0200

    Adjust output and iterations of the BinIndex evaluation

--- code/Evaluation/BinIndexEvaluation.cs
+++ code/Evaluation/BinIndexEvaluation.cs
@@ -17,7 +17,7 @@ public class BinIndexEvaluation
         Console.WriteLine("Create intervals");
         var intervals = new List<(double, double)>();
         var random = new Random();
-        for (var i = 0; i < 200_000; i++)
+        for (var i = 0; i < 1_000_000; i++)
         {
             intervals.Add((random.NextDouble()*100, random.NextDouble()*100));
         }
@@ -25,11 +25,12 @@ public class BinIndexEvaluation
         Console.WriteLine("Start evaluating");
         var stopwatch = new Stopwatch();
 
-        var numberOfWarmups = 3;
-        var numberOfEvaluations = 10;
+        var numberOfWarmups = 2;
+        var numberOfEvaluations = 3;
         Console.WriteLine("Start iterations");
         for (int i = 0; i < numberOfWarmups+numberOfEvaluations; i++)
         {
+            Console.WriteLine($"Iteration: {i-numberOfWarmups} / {numberOfEvaluations}");
             PerformEvaluations(stopwatch, intervals, measurements);
             if (i == numberOfWarmups - 1)
             {
@@ -56,6 +57,7 @@ public class BinIndexEvaluation
         // Own implementation
         //
         name = "BinIndex (filling)";
+        Console.WriteLine($"  Step: {name}");
         stopwatch.Start();
         var binIndex = new BinIndex<(double, double)>(100);
         for (var i = 0; i < intervals.Count - 1; i++)
@@ -68,6 +70,7 @@ public class BinIndexEvaluation
         stopwatch.Reset();
         
         name = "BinIndex (querying)";
+        Console.WriteLine($"  Step: {name}");
         stopwatch.Start();
         for (var i = 0; i < intervals.Count - 1; i++)
         {
@@ -84,6 +87,7 @@ public class BinIndexEvaluation
         // BinTree
         //
         name = "Bintree (filling)";
+        Console.WriteLine($"  Step: {name}");
         stopwatch.Start();
         var binTree = new Bintree<(double, double)>();
         for (var i = 0; i < intervals.Count - 1; i++)
@@ -96,16 +100,18 @@ public class BinIndexEvaluation
         stopwatch.Reset();
         
         name = "Bintree (querying)";
+        Console.WriteLine($"  Step: {name}");
+        var f = 100;
         stopwatch.Start();
-        for (var i = 0; i < intervals.Count - 1; i++)
+        for (var i = 0; i < intervals.Count/f - 1; i++)
         {
-            binTree.Query(intervals[i].Item1);
+            // binTree.Query(intervals[i].Item1);
             binTree.Query((intervals[i].Item1+intervals[i].Item2)/2);
-            binTree.Query(intervals[i].Item2);
+            // binTree.Query(intervals[i].Item2);
         }
         stopwatch.Stop();
         // Console.WriteLine($"{name} : {stopwatch.ElapsedMilliseconds}");
-        measurements[name] = measurements.TryGetValue(name, out measurement) ? measurement + GetMicroseconds(stopwatch.ElapsedTicks) : GetMicroseconds(stopwatch.ElapsedTicks);
+        measurements[name] = measurements.TryGetValue(name, out measurement) ? measurement + GetMicroseconds(stopwatch.ElapsedTicks*f) : GetMicroseconds(stopwatch.ElapsedTicks*f);
         stopwatch.Reset();
     }
 
